ea0a4c02248a25bee178e8dc20d236e7e06023ff
samroz-burhan <samroz814@gmail.com>
2026-07-15 14:52:28 +0500

    Frontend: Logo Fixes

--- radisist/radisist-frontend/src/LandingPage/Components/Logo.jsx
+++ radisist/radisist-frontend/src/LandingPage/Components/Logo.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import logo from "../../Images/Logo.png";
 
-export default function Logo({ className = "", menuOpen = false, darkBackground = false }) {
-  const showWhiteBg = darkBackground || menuOpen;
+export default function Logo({ className = "", menuOpen = false, darkBackground = false, white = false }) {
+  const showWhiteBg = (darkBackground || menuOpen) && !white;
 
   return (
     <div className={`flex items-center select-none transition-all duration-300 ${
       showWhiteBg ? "bg-white rounded-2xl p-1.5" : "bg-transparent"
     } ${className}`}>
-      <img src={logo} alt="Logo" className="h-10 w-auto md:h-12 object-contain" />
+      <img
+        src={logo}
+        alt="Logo"
+        className={`h-8 w-auto md:h-10 object-contain ${
+          white || darkBackground ? "brightness-0 invert" : ""
+        }`}
+      />
     </div>
   );
 }
